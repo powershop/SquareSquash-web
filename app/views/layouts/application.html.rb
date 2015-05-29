1d6f27213179dffcee0e6f29bef810db62dfb78e
mexisme <wildjim+dev@kiwinet.org>
2015-05-29 16:05:04 +1200

    Get Squash working with Google Auth
    Initial code for getting Squash working with Google Auth
    
    Add Google Auth to setup.rb
    
    Add third-party-login before-filter
    
    Fix log-out button
    
    Improve the doc-comments
    
    Much better implementation for managing 3rd-party logins
    
    Fix session_controller specs
    
    Fix the login-required tests
    
    Docs
    
    Enable Google OAuth and dot-env for Powershop
    
    Default lambda checks for allowed Google domains
    
    Change default authentication to Google OAuth

--- app/views/layouts/application.html.rb
+++ app/views/layouts/application.html.rb
@@ -16,176 +16,176 @@
 
 # @private
 module Views
+# @private
+module Layouts
   # @private
-  module Layouts
-    # @private
-    class Application < Erector::Widget
-      def content
-        rawtext "<!DOCTYPE html>"
-        html(lang: 'en') do
-          head_portion
-          body_portion
+  class Application < Erector::Widget
+    def content
+      rawtext "<!DOCTYPE html>"
+      html(lang: 'en') do
+        head_portion
+        body_portion
+      end
+    end
+
+    protected
+
+    # Override this method with your web page content.
+    def body_content
+      raise NotImplementedError
+    end
+
+    # Override this method to return `true` if you are using
+    # {#tabbed_section}. Sets the proper body background color so as to bleed
+    # the tab background color all the way to the bottom.
+    def tabbed?() false end
+
+    # Call as part of {#body_content} to generate a full-width
+    # (sixteen-column) content area with a white background. Yields to render
+    # content.
+    def full_width_section(alt=false)
+      div(class: "content-container#{alt ? '-alt' : ''}") do
+        div(class: 'container') do
+          div(class: 'row') { div(class: 'sixteen columns') { yield } }
+        end
+      end
+    end
+
+    # Call as part of {#body_content} to generate a full-width
+    # (sixteen-column) content area with a shaded background below a tabbed
+    # header portion. The `tabs_proc` should render a `<UL>` with your tab
+    # headers, and the `content_proc` should render the tab bodies. See
+    # tabs.js.coffee for how to organize it.
+    def tabbed_section(tabs_proc, content_proc)
+      div(class: 'tab-header-container') do
+        div(class: 'container') do
+          div(class: 'row') { div(class: 'sixteen columns') { tabs_proc.() } }
         end
       end
 
-      protected
-
-      # Override this method with your web page content.
-      def body_content
-        raise NotImplementedError
+      div(class: 'tab-container') do
+        div(class: 'container') do
+          div(class: 'row') { div(class: 'sixteen columns') { content_proc.() } }
+        end
       end
+    end
 
-      # Override this method to return `true` if you are using
-      # {#tabbed_section}. Sets the proper body background color so as to bleed
-      # the tab background color all the way to the bottom.
-      def tabbed?() false end
-
-      # Call as part of {#body_content} to generate a full-width
-      # (sixteen-column) content area with a white background. Yields to render
-      # content.
-      def full_width_section(alt=false)
-        div(class: "content-container#{alt ? '-alt' : ''}") do
-          div(class: 'container') do
-            div(class: 'row') { div(class: 'sixteen columns') { yield } }
+    # Call as part of {#body_content} to generate an inset, shaded
+    # twelve-column content area simulating the appearance of a modal. Yields
+    # to render content.
+    def modal_section
+      div(class: 'content-container') do
+        div(class: 'container modal-container') do
+          div(class: 'row row-modal') do
+            div(class: 'two columns') { text! '&nbsp;' }
+            div(class: 'twelve columns') { yield }
+            div(class: 'two columns') { text! '&nbsp;' }
           end
         end
       end
+    end
 
-      # Call as part of {#body_content} to generate a full-width
-      # (sixteen-column) content area with a shaded background below a tabbed
-      # header portion. The `tabs_proc` should render a `<UL>` with your tab
-      # headers, and the `content_proc` should render the tab bodies. See
-      # tabs.js.coffee for how to organize it.
-      def tabbed_section(tabs_proc, content_proc)
-        div(class: 'tab-header-container') do
-          div(class: 'container') do
-            div(class: 'row') { div(class: 'sixteen columns') { tabs_proc.() } }
-          end
-        end
+    # Override this method to customize the title tag.
+    def page_title() nil end
 
-        div(class: 'tab-container') do
-          div(class: 'container') do
-            div(class: 'row') { div(class: 'sixteen columns') { content_proc.() } }
-          end
+    # Override this method with the chain of objects that corresponds to the
+    # user's position in the navigational structure. For instance, if the user
+    # is viewing a bug within an environment within a Project, you'd want to
+    # return `[@project, @environment, @bug]`. This is used to build the
+    # breadcrumbs section.
+    #
+    # @return [Array] The nested objects forming the navigational position of
+    #   the current page.
+    def breadcrumbs() [] end
+
+    # Override this method to populate a set of at-a-glance numerical stats
+    # that will appear alongside the breadcrumbs. This method should returrn
+    # an array of arrays. The values of each inner array should be
+    #
+    # 1. the number to display,
+    # 2. a description of what the number represents, in singular, and
+    # 3. optionally, the plural form of #2.
+    #
+    # @return [Array<Array>] The stats to display alongside the breadcrumbs.
+    def breadcrumbs_stats()
+      []
+    end
+
+    ### HELPERS
+
+    # Like link_to, but makes a button. This much simpler version does not
+    # wrap it in a form, but instead uses the buttons.js.coffee file to add
+    # `<A>` behavior to it.
+
+    def button_to(name, location, overrides={})
+      button name, overrides.reverse_merge(href: location)
+    end
+
+    private
+
+    def head_portion
+      metas
+      page_title ?
+        title("Squash | #{page_title}") :
+        title("Squash: A Squarish exception tool")
+
+      stylesheet_link_tag 'application'
+      inline_css
+      comment('[if lt IE 9]') { javascript_include_tag 'http://html5shim.googlecode.com/svn/trunk/html5.js' }
+      comment('[if lt IE 8]') { javascript_include_tag 'flot/excanvas' }
+    end
+
+    def metas
+      meta charset: 'utf-8'
+      meta name: 'description', content: "A Squarish bug reporting, analytics, and management tool."
+      meta name: 'author', content: "Square, Inc."
+      meta name: 'viewport', content: 'width=device-width, initial-scale=1.0'
+      favicon_link_tag
+      csrf_meta_tags
+    end
+
+    def body_portion
+      body(class: "#{controller_name} #{tabbed? ? 'tabbed' : nil}", id: [controller_name, action_name].join('-')) do
+        div(id: 'navbar-container') do
+          navbar_small
+          navbar_large
         end
+        div(id: 'breadcrumbs-container') do
+          div(class: 'container') do
+            render_breadcrumbs(*breadcrumbs)
+            stats = breadcrumbs_stats
+            render_breadcrumbs_stats(stats) unless stats.empty?
+          end
+        end if breadcrumbs.present?
+
+        body_content
+        footer_portion
+
+        div id: 'flashes'
+
+        javascript_include_tag 'application'
+        inline_javascript
+        jquery(                                    "new Flash('alert'  ).text(\"#{escape_javascript flash[:alert]  }\")")     if flash[:alert]
+        jquery("$(window).oneTime(250, function() { new Flash('notice' ).text(\"#{escape_javascript flash[:notice] }\"); })") if flash[:notice]
+        jquery("$(window).oneTime(500, function() { new Flash('success').text(\"#{escape_javascript flash[:success]}\"); })") if flash[:success]
       end
+    end
 
-      # Call as part of {#body_content} to generate an inset, shaded
-      # twelve-column content area simulating the appearance of a modal. Yields
-      # to render content.
-      def modal_section
-        div(class: 'content-container') do
-          div(class: 'container modal-container') do
-            div(class: 'row row-modal') do
-              div(class: 'two columns') { text! '&nbsp;' }
-              div(class: 'twelve columns') { yield }
-              div(class: 'two columns') { text! '&nbsp;' }
+    def navbar_large
+      nav(class: 'container full-size-only') do
+        ul do
+          li(id: 'logo') do
+            link_to(root_url) do
+              image_tag 'bug.png'
+              text "Squash"
             end
           end
-        end
-      end
-
-      # Override this method to customize the title tag.
-      def page_title() nil end
-
-      # Override this method with the chain of objects that corresponds to the
-      # user's position in the navigational structure. For instance, if the user
-      # is viewing a bug within an environment within a Project, you'd want to
-      # return `[@project, @environment, @bug]`. This is used to build the
-      # breadcrumbs section.
-      #
-      # @return [Array] The nested objects forming the navigational position of
-      #   the current page.
-      def breadcrumbs() [] end
-
-      # Override this method to populate a set of at-a-glance numerical stats
-      # that will appear alongside the breadcrumbs. This method should returrn
-      # an array of arrays. The values of each inner array should be
-      #
-      # 1. the number to display,
-      # 2. a description of what the number represents, in singular, and
-      # 3. optionally, the plural form of #2.
-      #
-      # @return [Array<Array>] The stats to display alongside the breadcrumbs.
-      def breadcrumbs_stats()
-        []
-      end
-
-      ### HELPERS
-
-      # Like link_to, but makes a button. This much simpler version does not
-      # wrap it in a form, but instead uses the buttons.js.coffee file to add
-      # `<A>` behavior to it.
-
-      def button_to(name, location, overrides={})
-        button name, overrides.reverse_merge(href: location)
-      end
-
-      private
-
-      def head_portion
-        metas
-        page_title ?
-          title("Squash | #{page_title}") :
-          title("Squash: A Squarish exception tool")
-
-        stylesheet_link_tag 'application'
-        inline_css
-        comment('[if lt IE 9]') { javascript_include_tag 'http://html5shim.googlecode.com/svn/trunk/html5.js' }
-        comment('[if lt IE 8]') { javascript_include_tag 'flot/excanvas' }
-      end
-
-      def metas
-        meta charset: 'utf-8'
-        meta name: 'description', content: "A Squarish bug reporting, analytics, and management tool."
-        meta name: 'author', content: "Square, Inc."
-        meta name: 'viewport', content: 'width=device-width, initial-scale=1.0'
-        favicon_link_tag
-        csrf_meta_tags
-      end
-
-      def body_portion
-        body(class: "#{controller_name} #{tabbed? ? 'tabbed' : nil}", id: [controller_name, action_name].join('-')) do
-          div(id: 'navbar-container') do
-            navbar_small
-            navbar_large
-          end
-          div(id: 'breadcrumbs-container') do
-            div(class: 'container') do
-              render_breadcrumbs(*breadcrumbs)
-              stats = breadcrumbs_stats
-              render_breadcrumbs_stats(stats) unless stats.empty?
-            end
-          end if breadcrumbs.present?
-
-          body_content
-          footer_portion
-
-          div id: 'flashes'
-
-          javascript_include_tag 'application'
-          inline_javascript
-          jquery(                                    "new Flash('alert'  ).text(\"#{escape_javascript flash[:alert]  }\")")     if flash[:alert]
-          jquery("$(window).oneTime(250, function() { new Flash('notice' ).text(\"#{escape_javascript flash[:notice] }\"); })") if flash[:notice]
-          jquery("$(window).oneTime(500, function() { new Flash('success').text(\"#{escape_javascript flash[:success]}\"); })") if flash[:success]
-        end
-      end
-
-      def navbar_large
-        nav(class: 'container full-size-only') do
-          ul do
-            li(id: 'logo') do
-              link_to(root_url) do
-                image_tag 'bug.png'
-                text "Squash"
-              end
-            end
-            if logged_in?
-              navbar_projects('large')
-              navbar_environments('large') if @project
-              li(id: 'quicknav-container') { input type: 'search', id: 'quicknav', placeholder: 'Search' }
-              li { link_to "My Account", account_url }
-              li { link_to(logout_url) { i class: 'fa fa-sign-out'} }
+          if logged_in?
+            navbar_projects('large')
+            navbar_environments('large') if @project
+            li(id: 'quicknav-container') { input type: 'search', id: 'quicknav', placeholder: 'Search' }
+            li { link_to "My Account", account_url }
+            li { link_to(logout_url) { i class: 'fa fa-sign-out'} }
             else
               li(id: 'quicknav-container') { text! '&nbsp;' }
               li { link_to "Log In", login_url }
